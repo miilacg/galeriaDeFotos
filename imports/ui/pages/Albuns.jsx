e6823a658a2f45ca68dbf0cd58657d264d5e8174
Camila Guimarães <camiilacguimaraes@gmail.com>
2022-01-24 23:30:38 -0300

    ALBUNS: botão de criar novo álbum

--- imports/ui/pages/Albuns.jsx
+++ imports/ui/pages/Albuns.jsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useTracker } from 'meteor/react-meteor-data';
 
+import Button from '@mui/material/Button';
+//import TextField from '@mui/material/TextField';
+//import Typography from '@mui/material/Typography';
+
+import { CreateAlbum } from '../components/CreateAlbum/CreateAlbum';
 import { Header } from '../components/Header/Header';
 
 
 export function Albuns() {
   const user = useTracker(() => Meteor.user());
 
+  const [openCreateAlbum, setOpenCreateAlbum] = useState(false);
+
+
+  const handleOpenCreateAlbum = () => {
+    setOpenCreateAlbum(true);
+  };
+
+
   return (
     <div>
       { user ? (
-        <Header page='album' user={ user }/>
+        <>
+          <Header page='album' user={ user }/>
+
+          { openCreateAlbum && <CreateAlbum setOpenCreateAlbum={ setOpenCreateAlbum }/> }
+          
+          <Button variant="contained" onClick={ handleOpenCreateAlbum }>Criar novo álbum</Button>
+        </>
       ) : (
         <h1>Carregando...</h1>
       )}      
